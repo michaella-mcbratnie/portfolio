a3f7dd04ddefa0b63cf66c5e02741b61319e6086
michaella-mcbratnie <68383701+michaella-mcbratnie@users.noreply.github.com>
2020-09-24 12:39:04 -0400

    Update MathSciStudentCount_LgScale.sql

--- SQL Projects/MathSciStudentCount_LgScale.sql
+++ SQL Projects/MathSciStudentCount_LgScale.sql
@@ -25,7 +25,7 @@ SET STATISTICS TIME ON
   FROM Posts
   GROUP BY DATEPART(Year, CreationDate), DATENAME(Month, CreationDate)
   HAVING DATENAME(month,CreationDate) IN ('June', 'July', 'August')
-  --Results 1 = CPU Time: 23218 ms, Elapsed Time: 78522 ms
+  --Results 1 = CPU Time: 17,980 ms, Elapsed Time: 4,691 ms
 
 --Test Query 2: Filtering using WHERE (More appropriate since Month is not an Aggregated value)
   SELECT DATEname(month, CreationDate) AS Month, 
@@ -34,16 +34,7 @@ SET STATISTICS TIME ON
   FROM Posts
   WHERE DATENAME(month,CreationDate) IN ('June', 'July', 'August')
   GROUP BY DATEPART(Year, CreationDate) , DATENAME(Month, CreationDate) 
-  --Results 2 = CPU Time: 21624 ms (used less cpu), Elapsed Time: 78413 ms  
-
---Test Query 3: Removing the month value and aggregating by year == "Course Prefix Variable" and only provide the aggregate number of STEM enrollments.
-  SELECT DatePart(Year, CreationDate) AS Year,
-	  COUNT(Id)
-  FROM Posts 
-  WHERE DATENAME(Month, CreationDate) in ('July', 'June', 'August')
-  GROUP BY DATEPART(Year, CreationDate)
-  --RESULTS 3 = CPU Time: 18171 ms , Elapsed Time: 78507 ms
-  --Obviously this would create a query that produces results that are not separated by "Month" or Discipline.
+  --Results 2 = CPU Time: 17,818 ms (used less cpu), Elapsed Time: 4,750 ms  
 
 --Edited Original Query: changed HAVING to a WHERE statement.
   SELECT term, 
@@ -53,15 +44,44 @@ SET STATISTICS TIME ON
   WHERE LEFT(course, 3) in ('MAT', 'BIO', 'CHE', 'PHY', 'GSC') --Change from HAVING to WHERE to filter
   GROUP BY term, LEFT(course, 3)
   ORDER BY term, Course_prefix 
+  
+--Test Query 3: Removing the month value and aggregating by year == "Course Prefix Variable" and only provide the aggregate number of STEM enrollments.
+  SELECT DatePart(Year, CreationDate) AS Year,
+	  COUNT(Id)
+  FROM Posts 
+  WHERE DATENAME(Month, CreationDate) in ('July', 'June', 'August')
+  GROUP BY DATEPART(Year, CreationDate)
+  --RESULTS 3 = CPU Time: 18171 ms , Elapsed Time: 78507 ms (These numbers reflect querying a database with a malaligned index, therefore they no longer compare to the above.)
+  --Obviously this would create a query that produces results that are not separated by "Month" or Discipline.
+	--Edited Original Query 
+	SELECT Term, count(student_id) as StudentCounts
+	FROM CourseRegistration
+	WHERE Course IN ('MAT%', 'BIO%', 'CHE%', 'PHY%', 'GSC%')
+	GROUP BY Term
+	
+/* Potential Alternative for the above query that removes the need to use any formatting functions to create the desired data. This would require
+potentially adjustments to the database to have a CourseDescription table.*/
 
-/* Potential Alternative but adjustments still need to be made to the StackOverFlow Query since CreationDate/MonthofPost creates a
-table of data that has too many records and is not comparable to the function of these separate table/Join*/
+--Test Query 4: Join instead of formatting functions. A temp. table (#1) was created with Distinct CreationDate_Date(Date), MonthofPost (month datepart), Year (Yr Datepart)
+	SELECT MonthofPost, 
+		yearofpost,
+		COUNT(Id) as TotalPosts
+	FROM Posts as p
+	LEFT JOIN #1
+		on p.CreationDate_Date = #1.Dateofpost
+	WHERE MonthofPost IN ('June', 'July', 'August')
+	GROUP BY YearofPost, MonthofPost
+	--Results 4 = CPU time 11,592 ms, Elapsed Time = 3,342 ms.
 
   SELECT term,  
 	  d.Course_Prefix, 
 	  Count(StudentId) as StudentCounts
   FROM CourseRegistration as c
-  LEFT JOIN CourseDescription as d --joining allows us to avoid manipulating the course column data to generate a course_prefix
+  LEFT JOIN CourseDescription as d --joining allows us to avoid manipulating the data from the Course column to generate a course_prefix
   	ON c.Course = d.CourseCode
   WHERE d.Course_Prefix in ('MAT', 'BIO', 'CHE', 'PHY', 'GSC')
   GROUP BY term, d.Course_prefix
+
+SET STATISTICS TIME OFF
+  
+  
